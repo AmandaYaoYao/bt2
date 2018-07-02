c86041e7f7fcd1e9cdda0f89212610cc641950d8
Matt Sciamanna <msciamanna@college.harvard.edu>
2018-07-02 14:05:57 -0700

    changed a shitload of stuff definitely gonna have something go wrong

--- bt2.py
+++ bt2.py
@@ -29,6 +29,10 @@ class BluePlayer():
     state = None
     status = None
     track = []
+    player_list = []
+    connected2 = None
+    status2 = None
+    needs_flipped = False
 
     def __init__(self):
         """Specify a signal handler, and find any connected media players"""
@@ -47,6 +51,7 @@ class BluePlayer():
         self.findPlayer()
         self.updateDisplay()
 
+    #how do main loops work instead of other loop types? 
     def start(self):
         """Start the BluePlayer by running the gobject Mainloop()"""
         self.mainloop = gobject.MainLoop()
@@ -58,27 +63,22 @@ class BluePlayer():
             self.mainloop.quit();
     
     #Pseudo -- If player_path2 then we know there's 2 players
-    #we should check current signal -> Is it playing? 
-    #we should replace it with paused if it is
+        #we should replace it with paused if it is not 
         #We should let the thing know that we have this guy waiting to play next
         #We should look for signal that something has changed on player one (when next track happens)
         #Then we should play the player 2 song again
         #When should we add signal receiver to player 2? 
         #should we turn player 1 to player 2 and V.V. ? 
-        # WHat if we return from find player and call this if then player thing? 
+        #maybe add a nice needs flipped variable to tell if stuff needs to get flipped or not
     def ifPlayer(self, lst):
         player_path = lst[0]
         player_path2 = lst[1]
 
+        #Changed by getting rid of print(path2)
+        #Now making a call to player2
         if player_path2:
-           print(player_path2)
-           obj = self.bus.get_object('org.bluez', player_path2)
-           player_properties2 = obj.GetAll(PLAYER_IFACE, dbus_interface="org.freedesktop.DBus.Properties")
-           print('below here is obj.status')
-           if player_properties2["Status"] == 'playing':
-                print("they're both playing now")
-                obj.Pause(dbus_interface=PLAYER_IFACE)
-           print('END STATUS')
+           self.connected2 = True
+           self.player2(player_path2)
 
         if player_path:
                 self.connected = True
@@ -86,28 +86,48 @@ class BluePlayer():
                 player_properties = self.player.GetAll(PLAYER_IFACE, dbus_interface="org.freedesktop.DBus.Properties")
                 if "Status" in player_properties:
                     self.status = player_properties["Status"]
-                print('below is the original status')
+                print('below is player1s status')
                 print(self.status)
-                print('END GOOD STATUS')
                 if "Track" in player_properties:
                     self.track = player_properties["Track"]
 
+    #in playerhandler2 i set obj.connected, which probably should not happen realistically,
+    #means that we do not need to set
+    #gonna fuck round with these'r objects shortly and see what happens
+    def player2(path):
+           obj = self.bus.get_object('org.bluez', path)
+           player_properties2 = obj.GetAll(PLAYER_IFACE, dbus_interface="org.freedesktop.DBus.Properties")
+           if "Status" in player_properties2:
+                self.status2 = player_properties2["Status"]
+           if player_properties2["Status"] == 'playing':     
+                print("Just Paused Player 2")
+                obj.Pause(dbus_interface=PLAYER_IFACE)
+                print("This should be the status of player2 now:")
+                print(player_properties2["Status"])
+                self.needs_flipped = True
+                #does obj work here?
+           obj.bus.add_signal_receiver(self.playerHandler2,
+                    bus_name="org.bluez",
+                    dbus_interface="org.freedesktop.DBus.Properties",
+                    signal_name="PropertiesChanged",
+                    path_keyword="path")
+         
+    
+    #if there's not 2 devices device connected though, what are we going to do? 
+    #Should probably add a buttton for single player mode       
     def findPlayer(self):
         """Find any current media players and associated device"""
         manager = dbus.Interface(self.bus.get_object("org.bluez", "/"), "org.freedesktop.DBus.ObjectManager")
         objects = manager.GetManagedObjects()
 
-       #Literally no idea why its like this
-	player_list = []
+        #Literally no idea why its like this
         player_path = None
         for path, interfaces in objects.iteritems():
             if PLAYER_IFACE in interfaces:
-		#print(player_list)
-		#print(path)
-           	player_list += [path]
-
-        #this if player call calls the function in 
-        self.ifPlayer(player_list)
+                #indented this which was unexpected
+               	self.player_list += [path]
+        #this ifplayer call calls the function in 
+        self.ifPlayer(self.player_list)
 		
 
     def getPlayer(self, path):
@@ -124,8 +144,7 @@ class BluePlayer():
     def playerHandler(self, interface, changed, invalidated, path):
         """Handle relevant property change signals"""
         iface = interface[interface.rfind(".") + 1:]
-#        print("Interface: {}; changed: {}".format(iface, changed))
-
+        #print("Interface: {}; changed: {}".format(iface, changed))
         if iface == "Device1":
             if "Connected" in changed:
                 self.connected = changed["Connected"]
@@ -135,11 +154,37 @@ class BluePlayer():
                 if changed["Connected"]:
                     self.findPlayer()
         elif iface == "MediaPlayer1":
+            #when the track changes, check if we need to update player orders
             if "Track" in changed:
                 self.track = changed["Track"]
+                if needs_flipped == True:
+                    #NEED TO INTEGRATE THIS WITH FINDPLAYER WHICH HAS A FUCKED UP LIST ATM
+                    self.player_list = [self.player_list[0], self.player_list[1]]
+                    self.needs_flipped = False
+                    ifPlayer(self.player_list)
                 self.updateDisplay()
             if "Status" in changed:
                 self.status = (changed["Status"])
+
+    #Just made this noice old playerHandler2
+    def playerHandler2(self, interface, changed, invalidated, path):
+        """Handle relevant property change signals"""
+        iface = interface[interface.rfind(".") + 1:]
+        #print("Interface: {}; changed: {}".format(iface, changed))
+        if iface == "Device1":
+            if "Connected" in changed:
+                self.connected2 = changed["Connected"]
+        elif iface == "MediaControl1":
+            if "Connected" in changed:
+                self.connected2 = changed["Connected"]
+                if changed["Connected"]:
+                    self.findPlayer()
+        elif iface == "MediaPlayer1":
+            if "Status" in changed:
+                self.status2 = (changed["Status"])
+                self.needs_flipped = True
+                self.player2(self.player_list[1])
+
     def updateDisplay(self):
         if self.player:
             if "Artist" in self.track:
