2a448464e7c201dcb864de84d3c9b4a8a0af9158
Matt Sciamanna <msciamanna@college.harvard.edu>
2018-07-26 15:24:13 -0700

    addded three

---
+++ tests/hierarchy.py
@@ -0,0 +1,157 @@
+import pause
+
+#!/usr/bin/env python
+
+# Dependencies:
+# sudo apt-get install -y python-gobject
+# if name== main works for our subprocesses so no need to change
+# need to drop shit when ppl disconnect.
+"""
+CURRENT TESTS:
+Pause.run()
+Pause.end()
+
+"""
+
+import time
+import signal
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+import gobject
+
+
+ # to run play with async call back
+import threading
+from subprocess import Popen, PIPE
+
+SERVICE_NAME = "org.bluez"
+AGENT_IFACE = SERVICE_NAME + '.Agent1'
+ADAPTER_IFACE = SERVICE_NAME + ".Adapter1"
+DEVICE_IFACE = SERVICE_NAME + ".Device1"
+PLAYER_IFACE = SERVICE_NAME + '.MediaPlayer1'
+TRANSPORT_IFACE = SERVICE_NAME + '.MediaTransport1'
+
+class BluePlayer():
+    bus = None
+    mainloop = None
+    device = None
+    deviceAlias = None
+    player = None
+    connected = None
+    state = None
+    status = None
+    track = []
+    player_list = []
+    connected2 = None
+
+    def __init__(self):
+        """Specify a signal handler, and find any connected media players"""
+        gobject.threads_init()
+        # might ned this
+        dbus.mainloop.glib.threads_init()
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+
+        self.bus = dbus.SystemBus()
+
+        # self.bus.add_signal_receiver(self.playerHandler,
+        #         bus_name="org.bluez",
+        #         dbus_interface="org.freedesktop.DBus.Properties",
+        #         signal_name="PropertiesChanged",
+        #         path_keyword="path")
+
+        self.findPlayer()
+        self.updateDisplay()
+
+    def start(self):
+        """Start the BluePlayer by running the gobject Mainloop()"""
+        self.mainloop = gobject.MainLoop()
+        self.mainloop.run()
+
+    def end(self):
+        """Stop the gobject Mainloop()"""
+        if (self.mainloop):
+            self.mainloop.quit();
+
+    def findPlayer(self):
+        """Find any current media players and associated device"""
+        manager = dbus.Interface(self.bus.get_object("org.bluez", "/"), "org.freedesktop.DBus.ObjectManager")
+        objects = manager.GetManagedObjects()
+
+        for path, interfaces in objects.iteritems():
+            if PLAYER_IFACE in interfaces:
+                #indented this which was unexpected
+                self.player_list += [path]
+
+            player_path = None
+            player_path2 = None
+            #The 0 length case seems handled 
+            if len(self.player_list) ==  1: 
+              player_path = lst[0]
+        # this will break with three devices immediately
+            if len(self.player_list) == 2:
+              player_path = lst[0]
+              player_path2 = lst[1]
+
+        #Changed by getting rid of print(path2)
+        #Now making a call to player2
+        if player_path2:
+           self.connected2 = True
+           # make sure she woeks
+           pause.end()
+           # this guy makes the call to play
+           cmds = ["sudo","python", "play.py"] 
+		   cmds.append(player_path2)
+           popenAndCall(self.flipPlayer, cmds, shell=False, stdout=PIPE)
+
+        if player_path:
+            self.connected = True
+            pause.run(player_path)
+            time.sleep(10)
+            pause.end()
+
+    # obviously nonsensical at the moment.
+    def flipPlayer(self):
+    	if(player_list[2]):
+	    	popenAndCall(self.flipPlayer, player_list[1],)
+	    	pause.run(player_list[2])
+	    else:
+	    	popenAndCall(self.flipPlayer, player_list[1],)
+
+
+	def popenAndCall(onExit, *popenArgs, **popenKWArgs):
+	    """
+	    Runs a subprocess.Popen, and then calls the function onExit when the
+	    subprocess completes.
+
+	    Use it exactly the way you'd normally use subprocess.Popen, except include a
+	    callable to execute as the first argument. onExit is a callable object, and
+	    *popenArgs and **popenKWArgs are simply passed up to subprocess.Popen.
+	    """
+	    def runInThread(onExit, popenArgs, popenKWArgs):
+	        proc = Popen(*popenArgs, **popenKWArgs)
+	        proc.wait()
+	        onExit()
+	        return
+		
+	    thread = threading.Thread(target=runInThread,
+		                          args=(onExit, popenArgs, popenKWArgs))
+
+	    thread.start()
+
+	    return thread 
+	 
+
+if __name__ == "__main__":
+    player = None
+
+    try:
+        player = BluePlayer()
+        player.start()
+    except KeyboardInterrupt as ex:
+        print("\nBluePlayer cancelled by user")
+    except Exception as ex:
+        print("How embarrassing. The following error occurred {}".format(ex))
+    finally:
+        if player: player.end()
